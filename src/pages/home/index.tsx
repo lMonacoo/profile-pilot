a3dbbc64f980d3e9d9c319be2ffcd5dd2ed22ff9
lMonacoo <lucascansimonaco@yahoo.com.br>
2023-02-15 23:49:12 -0300

    feat: integrate all components in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,44 @@
+import { useState } from 'react'
+
+import {
+  AdapterUserApiResult,
+  OptionsUserList,
+  PortalModal,
+  SearchInput,
+  UserDetailed
+} from '~/components'
+import { useFetchUsers } from '~/hooks'
+import { UserModel } from '~/models'
+
 import GithubLogo from '../../assets/github.svg'
-import { SearchInput } from '../../components/search-input'
-import { OptionsUserList } from '../../components/options-user-list'
 import { Container, Logo } from './styles'
 
 export const Home = (): JSX.Element => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const { users, isLoading, error } = useFetchUsers(searchTerm)
+  const [detailedUser, setDetailedUser] = useState<UserModel | null>(null)
+
   return (
     <Container>
       <Logo src={GithubLogo} alt='Github Logo' />
       <SearchInput
         placeholder='Search a user'
-        onSearch={value => console.log(value)}
-        optionsListComponent={<OptionsUserList isLoading={false} />}
+        onSearch={setSearchTerm}
+        optionsListComponent={
+          <AdapterUserApiResult
+            isEmpty={users.length <= 0}
+            isLoading={isLoading}
+            isError={!!error}
+            isIdle={!searchTerm}
+          >
+            <OptionsUserList users={users} selectUser={user => setDetailedUser(user)} />
+          </AdapterUserApiResult>
+        }
       />
+
+      <PortalModal closeModal={() => setDetailedUser(null)} showModal={!!detailedUser}>
+        <UserDetailed user={detailedUser!} />
+      </PortalModal>
     </Container>
   )
 }
